591dd80318c40c6a658b9763b8399e67dcb1f673
Aryan Setia <aryansetiaa@gmail.com>
2024-07-26 11:10:15 +0530

    added consumer

--- producer.js
+++ producer.js
@@ -1,20 +1,36 @@
-const {kafka} = require("./client")
+const { kafka } = require("./client");
+const readline = require("readline");
 
-async function init(){
-    const producer = kafka.producer()
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-    console.log("connecting producer")
-    await producer.connect()
-    console.log("producer connected successfully")
+async function init() {
+  const producer = kafka.producer();
 
+  console.log("Connecting Producer");
+  await producer.connect();
+  console.log("Producer Connected Successfully");
+
+  rl.setPrompt("> ");
+  rl.prompt();
+
+  rl.on("line", async function (line) {
+    const [riderName, location] = line.split(" ");
     await producer.send({
-        topic: "rider-updates",
-       messages: [
-            {partition: 0, key: "location-update", value: JSON.stringify({name: "Tony Stark", location: "Hyderabad"})}
-        ]
-    })
-
-    await producer.disconnect()
+      topic: "rider-updates",
+      messages: [
+        {
+          partition: location.toLowerCase() === "north" ? 0 : 1,
+          key: "location-update",
+          value: JSON.stringify({ name: riderName, location }),
+        },
+      ],
+    });
+  }).on("close", async () => {
+    await producer.disconnect();
+  });
 }
 
 init();
